a88a2e7850708828381d5f7210722627aa9c4d7f
Tim <tim.gubner@cwi.nl>
2019-03-28 10:14:49 +0100

    Fixing expensiveOp experiment

--- scripts/expensiveop_exp.py
+++ scripts/expensiveop_exp.py
@@ -11,6 +11,8 @@ os.system('mkdir -p results/expensiveop')
 
 for s in [0, 10, 100, 1000, 10000]:
 	for gpu in gpu_values:
+		postfix = "gpu" if gpu is not None else "cpu"
+
 		file = "expensiveop/results-expensiveop_{}.csv".format(postfix)
 		run_test(fname=file, selectivity="1", slowdown=s)
 
